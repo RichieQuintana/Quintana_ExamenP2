7e4b425d930e23286458909eef88b429997eb418
Ricardo Quintana <ricardo.quintana.maldonado@udla.edu.ec>
2023-12-06 07:46:40 -0500

    Creamos una clase en la carpeta Models, la clase llamada Calculo que contiene propiedades que representan la información

---
+++ Models/Calculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quintana_AppApuntes.Models
+{
+    internal class Calculo
+    {
+        public int Numeros { get; set; }
+        public int Letras { get; set; }
+        public int Vocales { get; set; }
+        public int Mayusculas { get; set; }
+        public int Minusculas { get; set; }
+        public int TotalCaracteres { get; set; }
+    }
+}
